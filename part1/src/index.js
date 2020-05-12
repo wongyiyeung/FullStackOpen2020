81783f0d386bc2697186398b694d3bef2ca3244b
yiyeungwong <yiyeung@MacbookPro.local>
2020-05-13 00:32:32 +0800

    commit exercise 1-1

---
+++ part1/src/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.name}</h1>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return  (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  ) 
+}
+
+const Total = (props) => {
+  return (
+  <p>Number of exercises {props.total}</p>  
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header name={course} />
+      <Content part={part1} exercises={exercises1}/>
+      <Content part={part2} exercises={exercises2}/>
+      <Content part={part3} exercises={exercises3}/>
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
