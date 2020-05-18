b66a75a83525362d7ef1449595be6acf3101920f
yiyeungwong <yiyeung@MacbookPro.local>
2020-05-19 04:04:18 +0800

    commit for exercise 1-3

---
+++ part1/unicourse/src/index.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.name}</h1>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+    <Part name={props.module.name} exercises={props.module.exercises}/>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return (
+  <p>Number of exercises {props.total}</p>  
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  const parts = ['part1','part2','part3']
+  const exercises = [10,7,14]
+
+  return (
+    <div>
+      <Header name={course} />
+      <Content module={part1} />
+      <Content module={part2} />
+      <Content module={part3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
