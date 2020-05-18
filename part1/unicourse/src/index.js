f444594ca5d30e8c7f08ca920b9cd821d3f68127
yiyeungwong <yiyeung@MacbookPro.local>
2020-05-19 04:11:13 +0800

    commit for exercise 1-4

--- part1/unicourse/src/index.js
+++ part1/unicourse/src/index.js
@@ -12,7 +12,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-    <Part name={props.module.name} exercises={props.module.exercises}/>
+    <Part name={props.parts[0].name} exercises={props.parts[0].exercises}/>
+    <Part name={props.parts[1].name} exercises={props.parts[1].exercises}/>
+    <Part name={props.parts[2].name} exercises={props.parts[2].exercises}/>
     </>
   )
 }
@@ -27,34 +29,32 @@ const Part = (props) => {
 
 const Total = (props) => {
   return (
-  <p>Number of exercises {props.total}</p>  
+  <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>  
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  const parts = ['part1','part2','part3']
-  const exercises = [10,7,14]
+  const parts= [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header name={course} />
-      <Content module={part1} />
-      <Content module={part2} />
-      <Content module={part3} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
