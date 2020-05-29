a03bdaeb955404e0100d084dffcd9a1be52cbd96
yiyeungwong <yiyeung@MacbookPro.local>
2020-05-29 22:50:06 +0800

    commit for exercise 2-2

--- part2/unicourse/src/index.js
+++ part2/unicourse/src/index.js
@@ -33,7 +33,7 @@ const Part = (props) => (
 
 const Total = (props) => {
   return (
-  <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>  
+  <p><strong>Number of exercises:</strong> {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>  
   )
 }
 
