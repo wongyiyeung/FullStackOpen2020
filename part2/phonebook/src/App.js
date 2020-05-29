4bd09e6a541ce66c0fc6be364e848d9c26ab0c94
yiyeungwong <yiyeung@MacbookPro.local>
2020-05-30 02:39:37 +0800

    commit for exercise 2-9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,25 @@
 import React, { useState } from 'react'
 
-const Numbers = ({contactList}) => {
-return (
+const Numbers = ({contactList, filter}) => {
+  const filteredContacts = contactList.filter(person => person.name.slice(0, filter.length).localeCompare(filter, 'en', {sensitivity: 'accent'}) === 0);
+  return (
   <>
-    {contactList.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+    {filteredContacts.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
   </>
   )
 }
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('');
+  const [ filterName, setFilerName ] = useState('');
 
   const submitNewContact = (event) => {
     event.preventDefault()
@@ -43,9 +49,19 @@ const App = () => {
     setNewNumber(event.target.value);
   }
 
+  const changeFilter = (event) => {
+    setFilerName(event.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+        <form>
+          <div>
+            filter shown with <input onChange={changeFilter} value={filterName}/> 
+          </div>
+        </form>
+      <h2>Add new contact</h2>
       <form onSubmit={submitNewContact}>
         <div>
           name: <input onChange={changeName} value={newName}/>
@@ -58,7 +74,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Numbers contactList={persons} />
+      <Numbers contactList={persons} filter={filterName}/>
     </div>
   )
 }
