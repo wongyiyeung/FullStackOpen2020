ccfffbe3f9c9a5349e679ec4ab24e59eb02272d5
yiyeungwong <yiyeung@MacbookPro.local>
2020-06-11 21:56:28 +0800

    commit for exercise 2-11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const Filter = ({onChange, value}) => {
   return (
@@ -36,16 +37,19 @@ const Persons = ({contactList, filter}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('');
   const [ filterName, setFilerName ] = useState('');
+
+  useEffect(()=>{
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      })
+  },[]);
 
   const submitNewContact = (event) => {
     event.preventDefault()
