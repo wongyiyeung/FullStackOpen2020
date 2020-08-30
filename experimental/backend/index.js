6f6e897440ef1a5b91a614bbf86f5b8f7f14f334
yiyeung wong <wongyiyeung@gmail.com>
2020-08-31 00:48:22 +0800

    add cors to backend

--- experimental/backend/index.js
+++ experimental/backend/index.js
@@ -1,7 +1,9 @@
 const http = require('http')
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 let notes = [  
